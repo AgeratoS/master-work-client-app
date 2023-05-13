4289f777da45ed389127cb82a46db0ed2fa79b8c
thetiredsun <leshka99514@gmail.com>
2023-05-13 18:59:21 +0300

    Исправил проблему с @-нотацией на стороне Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -9,6 +11,18 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/nextjs",
     options: {},
+  },
+  webpackFinal: async (config, { configType }) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          '@': path.resolve(__dirname, '../src')
+        }
+      }
+    }
   },
   docs: {
     autodocs: "tag",
